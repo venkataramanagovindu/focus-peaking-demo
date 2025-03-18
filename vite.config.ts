26d9994f3966fab235a3704657c64ab76e3140f7
Venkata Ramana Govindu <venkataramanagovindu2015@gmail.com>
2025-03-17 23:34:09 -0700

    Update TypeScript and Vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  }, 
 })
+
